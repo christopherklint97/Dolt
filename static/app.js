7df50352e419ae7115769956164056ece6bc57f0
Christopher Klint <christopher.klint@gmail.com>
2020-11-30 13:08:34 +0100

    adding prettier to app.js

--- static/app.js
+++ static/app.js
@@ -1,144 +1,145 @@
 /* Datepicker from jQuery UI */
 $(function () {
-    $("#datepicker").datepicker({
-        dateFormat: "yy-mm-dd"
-    });
+  $("#datepicker").datepicker({
+    dateFormat: "yy-mm-dd",
+  });
 });
 
 /* Enable tooltips everywhere */
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
 /* Handle submission of the new task form */
 async function addNewTask(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const title = $('#new-task-title').val();
-    const description = $('#new-task-description').val();
-    const date = $('#datepicker').val();
-    const group = $('#new-task-group').val();
+  const title = $("#new-task-title").val();
+  const description = $("#new-task-description").val();
+  const date = $("#datepicker").val();
+  const group = $("#new-task-group").val();
 
-    // Send the new task to backend API
-    await axios.post('/api/tasks/new', {
-        title,
-        description,
-        date,
-        group
-    });
+  // Send the new task to backend API
+  await axios.post("/api/tasks/new", {
+    title,
+    description,
+    date,
+    group,
+  });
 
-    // Resetting of form values on submit
-    $('#new-task-title').val('');
-    $('#new-task-description').val('');
-    $('#datepicker').val('');
-    $('#new-task-group').val('');
+  // Resetting of form values on submit
+  $("#new-task-title").val("");
+  $("#new-task-description").val("");
+  $("#datepicker").val("");
+  $("#new-task-group").val("");
 
-    location.reload();
+  location.reload();
 }
 
 /* Handle submission of the add group form */
 async function addNewGroup(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const name = $('#new-group-name').val();
+  const name = $("#new-group-name").val();
 
-    // Send the new group to backend API
-    await axios.post('/api/groups/new', { name });
+  // Send the new group to backend API
+  await axios.post("/api/groups/new", { name });
 
-    // Resetting of form values on submit
-    $('#new-group-name').val('');
+  // Resetting of form values on submit
+  $("#new-group-name").val("");
 
-    location.reload();
+  location.reload();
 }
 
 /* Hide all of the new task fields */
 function hideNewTaskFields(e) {
-
-    if (e.target.tagName !== 'INPUT' && e.target.tagName !== 'SELECT' && e.target.tagName !== 'TEXTAREA') {
-        const newTaskFields = $('#new-task-fields');
-        newTaskFields.hide();
-    }
+  if (
+    e.target.tagName !== "INPUT" &&
+    e.target.tagName !== "SELECT" &&
+    e.target.tagName !== "TEXTAREA"
+  ) {
+    const newTaskFields = $("#new-task-fields");
+    newTaskFields.hide();
+  }
 }
 
 /* Show the new task fields on focus */
 function showNewTaskFields() {
-    const newTaskFields = $('#new-task-fields');
-    newTaskFields.show();
+  const newTaskFields = $("#new-task-fields");
+  newTaskFields.show();
 }
 
 /* Star task and send to API */
 async function starTask(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    id = e.currentTarget.attributes[1].value;
+  id = e.currentTarget.attributes[1].value;
 
-    await axios.post('/api/tasks/important', { id });
+  await axios.post("/api/tasks/important", { id });
 
-    location.reload();
+  location.reload();
 }
 
 /* Sort tasks in API and refresh */
 async function sortTasks(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    url = e.currentTarget.attributes[1].value;
+  url = e.currentTarget.attributes[1].value;
 
-    await axios.get(url);
+  await axios.get(url);
 
-    location.reload();
+  location.reload();
 }
 
 /* Complete tasks in API and refresh */
 async function completeTasks(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    console.log(e)
+  console.log(e);
 
-    id = e.currentTarget.attributes[1].value;
+  id = e.currentTarget.attributes[1].value;
 
-    await axios.post('/api/tasks/completed', { id });
+  await axios.post("/api/tasks/completed", { id });
 
-    location.reload();
+  location.reload();
 }
-
 
 /* Collection of all the event listeners */
 function addEventListeners() {
-    const newTaskForm = $('#new-task-form');
-    const newTaskTitle = $('#new-task-title');
-    const app = $('#app');
-    const cancelBtn = $('#cancel-btn');
-    const addGroupModal = $('#new-group-modal');
-    const addGroupForm = $('#add-group-form');
-    const star = $('[data-star]');
-    const sort = $('.sort');
-    const check = $('.check');
+  const newTaskForm = $("#new-task-form");
+  const newTaskTitle = $("#new-task-title");
+  const app = $("#app");
+  const cancelBtn = $("#cancel-btn");
+  const addGroupModal = $("#new-group-modal");
+  const addGroupForm = $("#add-group-form");
+  const star = $("[data-star]");
+  const sort = $(".sort");
+  const check = $(".check");
 
-    // Submit event for new tasks
-    newTaskForm.on('submit', addNewTask);
+  // Submit event for new tasks
+  newTaskForm.on("submit", addNewTask);
 
-    // Focus and click events for showing and hiding new task fields
-    newTaskTitle.on('focus', showNewTaskFields);
-    app.on('click', hideNewTaskFields);
-    cancelBtn.on('click', hideNewTaskFields);
+  // Focus and click events for showing and hiding new task fields
+  newTaskTitle.on("focus", showNewTaskFields);
+  app.on("click", hideNewTaskFields);
+  cancelBtn.on("click", hideNewTaskFields);
 
-    // Implement add group modal on click
-    addGroupModal.on('show.bs.modal', function () {
-        $('.edit-group-name').trigger('focus');
-    });
+  // Implement add group modal on click
+  addGroupModal.on("show.bs.modal", function () {
+    $(".edit-group-name").trigger("focus");
+  });
 
-    // Submit event for new groups
-    addGroupForm.on('submit', addNewGroup);
+  // Submit event for new groups
+  addGroupForm.on("submit", addNewGroup);
 
-    // Change importance for the task
-    star.on('click', starTask);
+  // Change importance for the task
+  star.on("click", starTask);
 
-    // Sort tasks based on backend API
-    sort.on('click', sortTasks);
+  // Sort tasks based on backend API
+  sort.on("click", sortTasks);
 
-    // Complete tasks by checking them
-    check.on('click', completeTasks);
-
+  // Complete tasks by checking them
+  check.on("click", completeTasks);
 }
 
 addEventListeners();
